ef392948cb12283ff3e812635fab7c202f0470e4
White Rabbit <47401029+rabbit949@users.noreply.github.com>
2025-01-13 04:22:23 +0000

    difference between having and where

--- MySQL for Data Analysis/5. Having vs Where.sql
+++ MySQL for Data Analysis/5. Having vs Where.sql
@@ -1,1 +1,16 @@
 # Having vs Where
+# so the main difference is, having clause is used to filter aggregate function level and where clause is use to row level, e.
+
+SELECT *
+FROM parks_and_recreation.employee_salary;
+
+SELECT occupation, avg(salary)
+FROM parks_and_recreation.employee_salary
+Group by occupation; 
+
+SELECT occupation, avg(salary)
+FROM parks_and_recreation.employee_salary
+where avg(salary) > 75000
+Group by occupation; # this will give us an error saying invalid use of group by statement, because the avg(salary) hasn't been created yet, so they created having clause to filter out the results
+
+
