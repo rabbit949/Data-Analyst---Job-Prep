1d8a80b2c52b5b268e77a66aea10f1758deb2567
White Rabbit <47401029+rabbit949@users.noreply.github.com>
2025-01-13 04:22:58 +0000

    How to use having clause in regard to where clasue

--- MySQL for Data Analysis/5. Having vs Where.sql
+++ MySQL for Data Analysis/5. Having vs Where.sql
@@ -13,4 +13,8 @@ FROM parks_and_recreation.employee_salary
 where avg(salary) > 75000
 Group by occupation; # this will give us an error saying invalid use of group by statement, because the avg(salary) hasn't been created yet, so they created having clause to filter out the results
 
-
+SELECT occupation, avg(salary)
+FROM parks_and_recreation.employee_salary
+where occupation LIKE '%manager%'
+Group by occupation
+having avg(salary) > 75000; # notice we get ocupation city manager with avg salary 90000
