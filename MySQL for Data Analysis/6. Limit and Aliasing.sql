25a0a3da448984df397318918643b6738d12cc7d
White Rabbit <47401029+rabbit949@users.noreply.github.com>
2025-01-13 04:46:20 +0000

    aliasing (changing the name of the column)

--- MySQL for Data Analysis/6. Limit and Aliasing.sql
+++ MySQL for Data Analysis/6. Limit and Aliasing.sql
@@ -16,3 +16,21 @@ Select *
 from parks_and_recreation.employee_demographics
 order by age desc
 limit 2, 1;  # this will only give back the last row of the oldest employee table we generated.
+
+
+-- ALIASING--
+# ALIASING is just a way to change the name of the column
+
+Select gender, avg(age)
+from parks_and_recreation.employee_demographics
+group by gender
+having avg(age) > 40; # this will create a column with the name of aggregate function and we don't want to show the funcation name, hence aliasing comes into play, we will be using it a lot more when we start using the joins on intermediate level.
+
+Select gender, avg(age) AS avg_age 
+from parks_and_recreation.employee_demographics
+group by gender
+having avg_age > 40;
+# this will change the name of the column from avg(age) to avg_age, we can name it whatever we want except for the reserved keywords, its like nameing a variable in programming language.
+
+
+ 
