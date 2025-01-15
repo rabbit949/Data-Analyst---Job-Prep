e67ad9c391342bc1b9fe517f283e43bded829644
White Rabbit <47401029+rabbit949@users.noreply.github.com>
2025-01-15 02:21:39 +0000

    outer joins (Left & Right)

--- MySQL for Data Analysis/7. Joins (Intermediate).sql
+++ MySQL for Data Analysis/7. Joins (Intermediate).sql
@@ -7,10 +7,20 @@ select * from parks_and_recreation.employee_demographics;
 select * from parks_and_recreation.employee_salary;
 
 select * from parks_and_recreation.employee_demographics
-join parks_and_recreation.employee_salary
+inner join parks_and_recreation.employee_salary
 	on employee_id = employee_id ; # this statement will give us error saying employee_id is ambigous meaning SQL deos not know where the employee_id is coming from so we have to specifiy where the column is coming from and notice both the columns have similar data id from 1-12;
     
 select * from parks_and_recreation.employee_demographics AS dem
-join parks_and_recreation.employee_salary AS sal
+inner join parks_and_recreation.employee_salary AS sal
 	on dem.employee_id = sal.employee_id ; #here employee_id 2 is not being populated because table dem does not have employee_id 2 w.r.t to table sal
 
+-- Outer Joins --
+-- Left Join --
+select * from parks_and_recreation.employee_demographics as dem
+left join parks_and_recreation.employee_salary as sal
+	on dem.employee_id = sal.employee_id ;  #left join basically takes everything and joins it to right table whereas in right table, it will populate all the row and if the rows doesn't match it will populate null
+
+-- Right -- 
+select * from parks_and_recreation.employee_demographics as dem
+right join parks_and_recreation.employee_salary as sal
+	on dem.employee_id = sal.employee_id ;  # the result will show table with null values in table 1 on row 2
